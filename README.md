f63d6eee02445fb60f2b231b6e6a0881a2d24349
Lars Nerger <lars.nerger@awi.de>
2025-06-19 08:38:43 +0200

    Update for PDAF3

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 NEMO-PDAF coupling routines. For the contributors to the code see MY_SRC_pdaf/initialize_pdaf.F90
 
-The code currently supports PDAF2 up to PDAF V2.3.1. It will be upgraded to PDAF3.
+The 'main' supports PDAF3. For PDAF2.3, use branch NEMO-PAF2.3
 
 For documentation on how to use the code, see the Wiki on https://github.com/PDAF/NEMO-PDAF.
 
--- offline_NEMO-PDAF/statevector_pdaf.F90
+++ offline_NEMO-PDAF/statevector_pdaf.F90
@@ -131,7 +131,15 @@ contains
 
 ! *** Local variables ***
     integer :: cnt               ! Counter for fields in the state vector
+#if defined key_top
     integer :: id_trc            ! Index of BGC (tracer) variables
+    namelist /state_vector/ screen, n_fields_covar, &
+         sv_temp, sv_salt, sv_ssh, sv_uvel, sv_vvel, &
+         sv_trc
+#else
+    namelist /state_vector/ screen, n_fields_covar, &
+         sv_temp, sv_salt, sv_ssh, sv_uvel, sv_vvel
+#endif
 
 
 ! **********************
@@ -151,15 +159,6 @@ contains
 #endif
 
 ! *** Read namelist file for state vector setup
-
-#if defined key_top
-    namelist /state_vector/ screen, n_fields_covar, &
-         sv_temp, sv_salt, sv_ssh, sv_uvel, sv_vvel, &
-         sv_trc
-#else
-    namelist /state_vector/ screen, n_fields_covar, &
-         sv_temp, sv_salt, sv_ssh, sv_uvel, sv_vvel
-#endif
 
     open (500,file='namelist_cfg.pdaf')
     read (500,NML=state_vector)
